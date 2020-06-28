dd577c70852168f551b77ad71d82bfffd201c2e7
YangNuoCheng <42427439+YangNuoCheng@users.noreply.github.com>
2020-06-28 15:04:55 +0800

    成功升级基地控制器

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,20 +1,19 @@
-var roleHarvester = {
+var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-     if(creep.store.getFreeCapacity() > 0) {
+     if(creep.store[RESOURCE_ENERGY] == 0) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
         else {
-            if(creep.transfer(Game.spawns['MyFirstTry'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['MyFirstTry']);
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
             }
         }
-    }
-    
+ }
 };
 
-module.exports = roleHarvester;
+module.exports = roleUpgrader;
